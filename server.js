2d0b00531ba62e60610b8b84c2a6bfcaf1dc473d
S. M. Abrar Jahin <abrarjahin@live.com>
2015-10-25 01:42:41 +0600

    first one

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+var http = require('http');
+
+http.createServer(function(request, responce)
+{
+	responce.writeHead(200,{'Content-Type' : 'text/html'});
+	responce.end('<html> <body> <h1>My Node.js site</h1> </body> </html>');
+}).listen(3000,'localhost');
+
+console.log("Server running on 'http://localhost:3000'");
+
+/*
 var connect = require('connect');
 
 var app = connect()
@@ -6,3 +17,5 @@ var app = connect()
 		res.end("Couldn't find it.");
 	})
 	.listen(3000);
+
+ */
